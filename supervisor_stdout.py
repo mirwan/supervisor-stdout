e7e6e3e54ae26b6cf76ef4322ba1914be4a5ea58
Erwan Miran <mirwan666@gmail.com>
2016-06-24 09:51:28 +0200

    Use of write_stderr and write_stdout depending on channel

--- supervisor_stdout.py
+++ supervisor_stdout.py
@@ -19,9 +19,7 @@ def main():
 def event_handler(event, response):
     line, data = response.split('\n', 1)
     headers = dict([ x.split(':') for x in line.split() ])
-    lines = data.split('\n')
-    prefix = '%s %s | '%(headers['processname'], headers['channel'])
-    print '\n'.join([ prefix + l for l in lines ])
+    eval("write_%s(%s)" % (headers['channel'],"'%s|%s'%(headers['processname'], data)"))
 
 if __name__ == '__main__':
     main()
